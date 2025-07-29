bfc22408425cd8fced56424f2bb2c201f619d849
WilsonYew <Wilsonyew38@gmail.com>
2025-07-29 11:46:45 +0800

    Initial commit

---
+++ build/ydlidar_sdk/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/golftrolley/robot_ws/src/ydlidar_sdk
+# Build directory: /home/golftrolley/robot_ws/build/ydlidar_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
