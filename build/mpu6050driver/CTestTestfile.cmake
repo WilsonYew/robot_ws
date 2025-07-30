523a47f7e68ef5869d60e6413674162886eba0c5
WilsonYew <Wilsonyew38@gmail.com>
2025-07-31 01:15:10 +0800

    mpu6050

---
+++ build/mpu6050driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/golftrolley/robot_ws/src/ros2_mpu6050_driver
+# Build directory: /home/golftrolley/robot_ws/build/mpu6050driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
