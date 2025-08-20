b47be7349d1d43efbe4d45870ef9921e1bcfec1c
WilsonYew <Wilsonyew38@gmail.com>
2025-08-20 13:54:44 +0800

    hardware interface

--- src/diffdrive_arduino-humble/hardware/include/diffdrive_arduino/diffbot_system.hpp
+++ src/diffdrive_arduino-humble/hardware/include/diffdrive_arduino/diffbot_system.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef ROS2_CONTROL_DEMO_EXAMPLE_2__DIFFBOT_SYSTEM_HPP_
-#define ROS2_CONTROL_DEMO_EXAMPLE_2__DIFFBOT_SYSTEM_HPP_
+#ifndef DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
+#define DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
 
 #include <memory>
 #include <string>
@@ -29,38 +29,78 @@
 #include "rclcpp/time.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "diffdrive_arduino/visibility_control.h"
 
-namespace ros2_control_demo_example_2
+#include "diffdrive_arduino/arduino_comms.hpp"
+#include "diffdrive_arduino/wheel.hpp"
+
+namespace diffdrive_arduino
 {
-class DiffBotSystemHardware : public hardware_interface::SystemInterface
+class DiffDriveArduinoHardware : public hardware_interface::SystemInterface
 {
+
+struct Config
+{
+  std::string left_wheel_name = "";
+  std::string right_wheel_name = "";
+  float loop_rate = 0.0;
+  std::string device = "";
+  int baud_rate = 0;
+  int timeout_ms = 0;
+  int enc_counts_per_rev = 0;
+  int pid_p = 0;
+  int pid_d = 0;
+  int pid_i = 0;
+  int pid_o = 0;
+};
+
+
 public:
-  RCLCPP_SHARED_PTR_DEFINITIONS(DiffBotSystemHardware)
+  RCLCPP_SHARED_PTR_DEFINITIONS(DiffDriveArduinoHardware);
 
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::CallbackReturn on_init(
     const hardware_interface::HardwareInfo & info) override;
 
+  DIFFDRIVE_ARDUINO_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  DIFFDRIVE_ARDUINO_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::CallbackReturn on_configure(
     const rclcpp_lifecycle::State & previous_state) override;
 
+  DIFFDRIVE_ARDUINO_PUBLIC
+  hardware_interface::CallbackReturn on_cleanup(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::return_type read(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
+  DIFFDRIVE_ARDUINO_PUBLIC
   hardware_interface::return_type write(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
-  // Parameters for the DiffBot simulation
-  double hw_start_sec_;
-  double hw_stop_sec_;
+
+  ArduinoComms comms_;
+  Config cfg_;
+  Wheel wheel_l_;
+  Wheel wheel_r_;
 };
 
-}  // namespace ros2_control_demo_example_2
+}  // namespace diffdrive_arduino
 
-#endif  // ROS2_CONTROL_DEMO_EXAMPLE_2__DIFFBOT_SYSTEM_HPP_
+#endif  // DIFFDRIVE_ARDUINO__DIFFBOT_SYSTEM_HPP_
